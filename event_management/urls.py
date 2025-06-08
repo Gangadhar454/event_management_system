d3295fcd081fa02bb85ff17edca199846746f87c
gangadhar <gangadhar@krinati.co>
2025-06-08 22:04:33 +0530

    swagger static files

--- event_management/urls.py
+++ event_management/urls.py
@@ -3,6 +3,8 @@ from event_management.views import event_views, attendee_views
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
 from rest_framework import permissions
+from django.conf import settings
+from django.conf.urls.static import static
 
 schema_view = get_schema_view(
     openapi.Info(
@@ -20,3 +22,6 @@ urlpatterns = [
     path('events/<str:event_id>/attendees/', attendee_views.EventAttendeesView.as_view(), name='event-attendees'),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
